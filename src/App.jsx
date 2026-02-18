39e5f1877bbdf72ba90c96c4ed8b2139adfcbeee
Camila Germano Rodrigues <camyrodrigues23@gmail.com>
2026-02-18 20:49:43 -0300

    feat: criacao projeto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <header>
+        <img src="/logo.png "alt=''/>
+      </header>
+      <section>
+        <img src="/banner.png"/>
+      </section>
+    </main>
+  )
+}
+
+export default App
